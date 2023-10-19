9d4304bbd246f8aa239a2234d2c9aca2eb4ba3a6
zokoriko <95735724+zokoriko@users.noreply.github.com>
2023-10-19 20:58:56 +1100

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xdF5A8D72C31Eb8638e65d8b7AA95A924036Ff857/g' config.conf
 sleep 5
 echo "---------------------------"
 
